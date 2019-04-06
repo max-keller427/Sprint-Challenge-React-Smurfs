8557ebffc7bb5a72dfbfd5052e82e33bde64178f
max keller <max.keller427@gmail.com>
2019-04-06 11:09:34 -0600

    form functionality working

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,7 +14,12 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    axios
+      .post("http://localhost:3333/smurfs", this.state)
+      .then(res => {
+        this.props.updateSmurfs(res.data);
+      })
+      .catch(err => console.log(err));
     this.setState({
       name: "",
       age: "",
